6147408045d3e17870e99a6cfe1c9c84f557852a
Frank <frankiek3@gmail.com>
2014-05-27 15:04:23 -0400

    Fix 'select2' select boxes when adding rows and change to append

--- app/assets/javascripts/spree/backend/spree_volume_pricing.js
+++ app/assets/javascripts/spree/backend/spree_volume_pricing.js
@@ -1,24 +1,30 @@
-// spree's version only handles 'input', not 'select', and this breaks spree_volume_pricing
+// spree's version handles 'input', and 'select', but then select2 doesn't work correctly
 
 $(function () {
-  $('.new_volume_price_row.add_fields').off('click').on('click', function() {
+  $('.spree_add_fields').off('click').on('click', function() {
     var target = $(this).data("target");
-    var new_table_row = $(target + ' tr:visible:last').clone();
+    var new_table_row = $(target + ' tr:first').clone();
     var new_id = new Date().getTime();
-    new_table_row.find("input,select").each(function () {
+    new_table_row.find("select").each(function () {
+      var el = $(this);
+      new_table_row.find("#s2id_"+el.prop('id')).remove();
+    });
+    new_table_row.find("input, select").each(function () {
       var el = $(this);
       el.val("");
-      el.attr("id", el.attr("id").replace(/\d+/, new_id))
-      el.attr("name", el.attr("name").replace(/\d+/, new_id))
+      el.prop("id", el.prop("id").replace(/\d+/, new_id))
+      el.prop("name", el.prop("name").replace(/\d+/, new_id))
     });
+    new_table_row.show();
     // When cloning a new row, set the href of all icons to be an empty "#"
     // This is so that clicking on them does not perform the actions for the
-    // duplicated row
+    // duplicated row (Warning: clears javascript in href)
     new_table_row.find("a").each(function () {
       var el = $(this);
-      el.attr('href', '#');
+      el.prop('href', '#');
     });
-    $(target).prepend(new_table_row);
+    $(target).append(new_table_row);
+    new_table_row.find('.select2').select2();
   });
 
 });
