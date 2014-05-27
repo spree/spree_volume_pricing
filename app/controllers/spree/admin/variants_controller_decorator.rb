289d4d8d10f15240e595f3bfc1592ec285b3019e
Frank <frankiek3@gmail.com>
2014-05-27 15:22:05 -0400

    Combine views and update to Spree usage along with drag sorting

--- app/controllers/spree/admin/variants_controller_decorator.rb
+++ app/controllers/spree/admin/variants_controller_decorator.rb
@@ -5,6 +5,17 @@ Spree::Admin::VariantsController.class_eval do
     @product = @variant.product
   end
 
+  def update_volume_price_positions
+    params[:positions].each do |id, index|
+      Spree::VolumePrice.find(id).update_attributes(:position => index)
+    end
+
+    respond_to do |format|
+      format.html { redirect_to spree.admin_product_volume_prices_path(@variant) }
+      format.js { render :text => 'Ok' }
+    end
+  end
+
   private
 
   # this loads the variant for the master variant volume price editing
--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -7,5 +7,5 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
 Deface::Override.new(:virtual_path => "spree/admin/variants/edit",
                      :name => "add_volume_pricing_field_to_variant",
                      :insert_after => "[data-hook='admin_variant_edit_form']",
-                     :partial => "spree/admin/variants/edit_fields",
+                     :partial => "spree/admin/variants/volume_pricing_edit",
                      :disabled => false)
