94f5adbcbf376bfe1f57eb80b2e3cf3a9c62fdfe
Jacob Spizziri <jacob.spizziri@gmail.com>
2015-02-25 08:34:37 -0600

    tidy up files

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -1,14 +1,15 @@
-Deface::Override.new(
-  virtual_path: 'spree/admin/shared/_product_tabs',
-  name: 'add_volume_pricing_admin_tab',
-  insert_bottom: '[data-hook="admin_product_tabs"]',
-  partial: 'spree/admin/shared/vp_product_tab'
-)
+Deface::Override.new(:virtual_path => "spree/admin/variants/edit",
+                     :name => "add_volume_pricing_model_field_to_variant",
+                     :insert_after => "[data-hook='admin_variant_edit_form']",
+                     :partial => "spree/admin/variants/edit_volume_price_model_fields",
+                     :disabled => false)
 
 Deface::Override.new(
-  virtual_path: "spree/admin/variants/edit",
-  name: "add_volume_pricing_model_field_to_variant",
-  insert_after: "[data-hook='admin_variant_edit_form']",
-  partial: "spree/admin/variants/edit_volume_price_model_fields",
-  disabled: false
-)
+  virtual_path:  'spree/admin/shared/_configuration_menu',
+  name:          'add_volume_price_model_admin_menu_links',
+  insert_bottom: "[data-hook='admin_configurations_sidebar_menu']"
+) do
+  <<-HTML
+    <%= configurations_sidebar_menu_item Spree.t('volume_price_models'), admin_volume_price_models_path %>
+  HTML
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,6 @@
 Spree::Core::Engine.add_routes do
   namespace :admin do
 
-    resources :volume_price_models
 
-    resources :products do
-       resources :variants do
-        get :volume_prices, on: :member
-      end
-    end
-
-    delete '/volume_prices/:id', to: 'volume_prices#destroy', as: :volume_price
   end
 end
