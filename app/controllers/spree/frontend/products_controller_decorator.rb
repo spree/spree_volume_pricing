f30c8e8a257fa78e7d89830af9a5ce37dc889abf
Jacob Spizziri <jacob.spizziri@gmail.com>
2015-04-10 11:23:46 -0500

    default role if no user

--- app/controllers/spree/frontend/products_controller_decorator.rb
+++ app/controllers/spree/frontend/products_controller_decorator.rb
@@ -3,7 +3,14 @@ Spree::ProductsController.class_eval do
 
   private
     def load_pricing_model
-      role = try_spree_current_user.resolve_role
+      user = try_spree_current_user
+
+      if user
+        role = try_spree_current_user.resolve_role
+      else
+        role = Spree::Role.find_by name: 'user'
+      end
+
       @volume_prices = @product.master.volume_prices(role)
     end
 
